53b6f773e8f56c396a8a3cccf52c6e1de7dcfdf7
Pablo <pablobarreiro131@gmail.com>
2025-05-26 10:52:33 +0200

    Ejercicio 28. Completado y testeado.

--- src/main/java/com/campusdual/classroom/Exercise28.java
+++ src/main/java/com/campusdual/classroom/Exercise28.java
@@ -2,6 +2,9 @@ package com.campusdual.classroom;
 
 public class Exercise28 {
     public static void main(String[] args) {
-
+        int[] array = new int[2];
+        for (int i = 0; i<5; i++){
+            array[i] = i;
+        }
     }
 }
